9867a1e6ee6590b85b39d7d89ba90841a100bb16
Alexis Torreno <alexis.torreno@gmail.com>
2015-01-01 10:46:24 -0500

    added Transaction resource

---
+++ api/models/Transaction.js
@@ -0,0 +1,41 @@
+/**
+* Transaction.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    transactionID: {
+      type: 'integer',
+      autoIncrement: true,
+      primaryKey: true
+    },
+    transactionTypeID: {
+      type: 'integer',
+      required: true
+    },
+    transactionDate: {
+      type: 'datetime',
+      required: true
+    },
+    vendorID: {
+      type: 'integer'
+    },
+    paymentType:{
+      type: 'string',
+      enum: ['credit', 'debit', 'cash']
+    },
+    amount: {
+      type: 'float',
+      required: true
+    },
+    note: {
+      type: 'string',
+      size: 255
+    }
+  }
+};
+
