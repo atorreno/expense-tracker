20bb99ac4bf26f92d71cfd118788b90209fe0323
Al Torreno <atorreno@klick.com>
2015-01-18 18:46:39 -0500

    trying to get openshift to work

--- config/local.js
+++ config/local.js
@@ -61,7 +61,7 @@ module.exports = {
    ***************************************************************************/
 
   // port: process.env.PORT || 1337,
-  host: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
+  host: process.env.OPENSHIFT_APP_DNS || "127.0.0.1",
   port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
   environment: process.env.NODE_ENV || 'development'
   /***************************************************************************
